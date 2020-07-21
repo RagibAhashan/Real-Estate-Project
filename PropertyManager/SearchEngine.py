6326ec96e73552a2352ab71b754a2fe3faa09c65
RagibAhashan <ahashanragib@gmail.com>
2020-07-21 02:16:33 -0400

    JSON support added

--- PropertyManager/SearchEngine.py
+++ PropertyManager/SearchEngine.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+import json
 
 def isLastPage(currentPage):
     x = currentPage
@@ -20,6 +21,7 @@ driver = webdriver.Chrome(PATH)
 driver.get('https://www.centris.ca/en/properties~for-sale?view=Thumbnail')
 
 try:
+    data = []
     input_area = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "select2-search__field"))
     )
@@ -48,6 +50,7 @@ try:
     element.click()
 
     select_boxes = ['Single-family home', 'Condo', 'Loft / Studio', 'Plex', 'Intergenerational']
+    select_boxes = ['Intergenerational']
     check_boxes = driver.find_elements_by_class_name("custom-control-label")
     time.sleep(1)
     for box in check_boxes:
@@ -63,6 +66,7 @@ try:
 
     time.sleep(3)
     n = 0
+
     while True:
         time.sleep(1)
         all_info = driver.find_elements_by_class_name("shell")
@@ -74,12 +78,19 @@ try:
         x = pages[0].text
         for i in range(len(price)):
             line = ', Price: {}, Category {}, address: {}'.format(price[i].text, category[i].text, address[i].text)
+            line.replace('\n', ' ')
             n += 1
             print('#', n, line, '\n\n')
+            
+            data.append({
+                'price': price[i].text,
+                'category': category[i].text,
+                'address': address[i].text
+            })
         currentPage = [int(s) for s in x.split() if s.isdigit()]
         time.sleep(1)
 
-        print(x, 'pages completed. ETA: ', "{:.0f".format(currentPage[1]*5/60), 'mins')
+        print(x, 'pages completed. ETA: ', round((currentPage[1]*5/60), 0), 'mins')
         time.sleep(1)
         if currentPage[0] == currentPage[1]:
             break
@@ -89,6 +100,9 @@ try:
         )
         element.click()
         time.sleep(1)
+
+    with open('data.txt', 'w') as outfile:
+        json.dump(data, outfile)
 
 
 
--- PropertyManager/test.py
+++ PropertyManager/test.py
@@ -1,3 +1,22 @@
-x = '1 / 254 '
-# print(x.find('/'), len(x)-1)
-print(int(x[(x.find('/')+2):(len(x)-1)]))
+import json
+
+data = {}
+data = []
+data.append({
+    'name': 'Scott',
+    'website': 'stackabuse.com',
+    'from': 'Nebraska'
+})
+data.append({
+    'name': 'Larry',
+    'website': 'google.com',
+    'from': 'Michigan'
+})
+data.append({
+    'name': 'Tim',
+    'website': 'apple.com',
+    'from': 'Alabama'
+})
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
