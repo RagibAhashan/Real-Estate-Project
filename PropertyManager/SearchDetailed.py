6d3b6c7d4108776d004bd5c7bd748c9902a92484
Ragib <ahashanragib@gmail.com>
2020-07-21 21:01:39 -0400

    Detailed search works

--- PropertyManager/SearchDetailed.py
+++ PropertyManager/SearchDetailed.py
@@ -8,107 +8,127 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import json
 from bs4 import BeautifulSoup
+import json
+
+def search():
+    data = []
+    PATH = os.getcwd() + '/chromedriver'
+    driver = webdriver.Chrome(PATH)
+    driver.get('https://www.centris.ca/en/properties~for-sale?view=Thumbnail')
+    data = []
+
+    try:
+        input_area = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "select2-search__field"))
+        )
+        input_area.send_keys('Montreal')
+        input_area.send_keys(Keys.RETURN)
+
+        time.sleep(2)
+        
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "select2-results__option"))
+        )
+        element.click()
+
+        time.sleep(2)
+
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "filter-search"))
+        )
+        element.click()
+        time.sleep(2)
+
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "PropertyTypeSection-accordion"))
+        )
+        element.click()
+
+        select_boxes = ['Single-family home', 'Condo', 'Loft / Studio', 'Plex', 'Intergenerational']
+        select_boxes = ['Intergenerational']
+
+        check_boxes = driver.find_elements_by_class_name("custom-control-label")
+        time.sleep(1)
+        for box in check_boxes:
+            if box.text in select_boxes:
+                box.click()
+        
+        time.sleep(2)
+
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "property-count"))
+        )
+        element.click()
+
+        time.sleep(3)
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "ButtonViewSummary"))
+        )
+        element.click()
+        time.sleep(2)
+
+        while True:
+            title_address    = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "address"))
+            )
+            price    = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "BuyPrice"))
+            )
+            pages    = driver.find_elements_by_class_name("pager-current")
+            time.sleep(1)
+            title, address = title_address.text.split('\n')[0], title_address.text.split('\n')[1]
+            js_data = {}
+            js_data["Property"] = title
+            js_data["Address"] = address
+            js_data["Price"] = price.text
+
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            divs1 = soup.findAll('div', {"class" : "carac-title"})
+            divs2 = soup.findAll('div', {"class" : "carac-value"})
+            time.sleep(3)
+
+            # Loads data to JSON Object for appending.
+            for i in range(len(divs1)):
+                js_data[str(divs1[i].get_text())] = str(divs2[i].get_text())
+            data.append(js_data)
+            print(data)
+
+            x = pages[0].text
+            # Checks if current page is final page...
+            pages = [int(s) for s in x.split() if s.isdigit()]
+            currentPage, lastPage = pages[0], pages[1]
+            time_left = round(((lastPage-currentPage)*5/60), 2)
+            seconds = int((time_left%1)*60)
+            if seconds/10 < 1:
+                seconds = '0' + str(seconds)
+            mins =  int(time_left)
+            time_left = str(mins) + ':' + str(seconds)
+            print(x, 'pages completed. ETA: ', time_left, 'mins')
+            time.sleep(1)
+            if currentPage == lastPage:
+                break
+            time.sleep(2)
+
+            # Goes to next page.
+            element = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "next"))
+            )
+            element.click()
+            time.sleep(1)
+        with open('assets.json', 'w') as outfile:
+                json.dump(data, outfile)
 
 
-PATH = os.getcwd() + '/chromedriver'
-driver = webdriver.Chrome(PATH)
-driver.get('https://www.centris.ca/en/properties~for-sale?view=Thumbnail')
-data = []
+    except:
+        print('something went wrong..\n Restarting...')
+        driver.quit()
+        search()
+    finally:
+        with open('data.txt', 'w') as outfile:
+            json.dump(data, outfile)
+        driver.quit()
+        print('DONE!')
 
-try:
-    input_area = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "select2-search__field"))
-    )
-    input_area.send_keys('Montreal')
-    input_area.send_keys(Keys.RETURN)
 
-    time.sleep(2)
-    
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "select2-results__option"))
-    )
-    element.click()
-
-    time.sleep(2)
-
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "filter-search"))
-    )
-    element.click()
-    #PropertyTypeSection-accordion
-    time.sleep(2)
-
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "PropertyTypeSection-accordion"))
-    )
-    element.click()
-
-    select_boxes = ['Single-family home', 'Condo', 'Loft / Studio', 'Plex', 'Intergenerational']
-
-    check_boxes = driver.find_elements_by_class_name("custom-control-label")
-    time.sleep(1)
-    for box in check_boxes:
-        if box.text in select_boxes:
-            box.click()
-    
-    time.sleep(2)
-
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "property-count"))
-    )
-    element.click()
-
-    time.sleep(3)
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "ButtonViewSummary"))
-    )
-    element.click()
-    time.sleep(2)
-    n = 0
-
-    # category = driver.find_elements_by_class_name("category")
-    # address  = driver.find_elements_by_class_name("address")
-
-    title_address    = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "address"))
-    )
-    price    = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "BuyPrice"))
-    )
-    pages    = driver.find_elements_by_class_name("pager-current")
-
-    # description    = WebDriverWait(driver, 10).until(
-    #     EC.presence_of_element_located((By.CLASS_NAME, "row teaser")) 
-    # )
-    # description2    = WebDriverWait(driver, 10).until(
-    #     EC.presence_of_element_located((By.CLASS_NAME, "row")) 
-    # )
-    time.sleep(1)
-    title, address = title_address.text.split('\n')[0], title_address.text.split('\n')[1]
-
-    print(title)
-    print(address)
-    print(price.text)
-    print(pages[0].text)
-    # print(driver.page_source)
-    f = open("sec.html", "a")
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    divs = soup.findAll('div', {"class" : "row teaser"})
-    divs1 = soup.findAll('div', {"class" : "carac-title"})
-    divs2 = soup.findAll('div', {"class" : "carac-value"})
-    # divs = soup.findAll('div', {"class" : "row teaser"})
-    time.sleep(3)
-    for i in range(len(divs1)):
-    	print(str(divs1[i]), str(divs2[i]))
-    # f.write(str(divs1) + str(divs2))
-    # f.close()
-    # print(description)
-    # print(description2)
-
-except:
-    print('something went wrong..')
-finally:
-    with open('data.txt', 'w') as outfile:
-        json.dump(data, outfile)
-    print('DONE!')
-    # driver.quit()
+if __name__ == "__main__":
+    search()
